353f8ff537e1f57a2342b1f3eedab9449e9bf2fb
James Nakagawa <jamesnakagawa@cfenet.ubc.ca>
2020-02-12 13:54:58 -0800

    Allow paths from other hostnames (incl. protocol)

--- src/generate-path.ts
+++ src/generate-path.ts
@@ -4,28 +4,35 @@ import {compile} from "path-to-regexp";
  * Taken from react-router
  *
  */
-const cache: Record<string, (a: object, b: object) => string> = {};
+const cache: Record<string, {protocolAndHostname: string, generator: (a: object, b: object) => string}> = {};
 const cacheLimit = 10000;
 let cacheCount = 0;
 
 function compilePath(path: string) {
+    let protocolAndHostname = '';
+    if (/^https?:\/\//.test(path)) {
+        [protocolAndHostname] = path.match(/^https?:\/\/[^\/@]+\//);
+        path = path.substr(protocolAndHostname.length);
+    }
+
     if (cache[path]) return cache[path];
 
     const generator = compile(path);
 
     if (cacheCount < cacheLimit) {
-        cache[path] = generator;
+        cache[path] = {protocolAndHostname, generator};
         cacheCount++;
     }
 
-    return generator;
+    return {protocolAndHostname, generator};
 }
 
 /**
  * Public API for generating a URL pathname from a path and parameters.
  */
 function generatePath(path: string = "/", params = {}) {
-    return path === "/" ? path : compilePath(path)(params, { pretty: true });
+    let {protocolAndHostname, generator} = compilePath(path);
+    return path === "/" ? path : (protocolAndHostname + generator(params, { pretty: true }));
 }
 
 export default generatePath;
